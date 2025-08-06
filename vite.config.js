2273b70297bb79ff3b9727f3ec6e6ee789934c74
E.R.T-HASAN <mdwasimhasan454@gmail.com>
2025-08-06 11:58:17 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react(), ],
+  base: '/personal-portfolio/', 
+  plugins: [tailwindcss(), react()],
   server: {
-    open: true, // यदि यह true है तो यह default ब्राउज़र में खोलेगा
+    open: true,
     host: true
   }
 })
